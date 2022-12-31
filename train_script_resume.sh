dadb5c525ed33d684a42e7a8dc7077ea26660e25
Hamidreza <hamidreza.hashempour@yahoo.com>
2022-12-31 21:51:04 +0900

    Update train_script_resume.sh

--- train_script_resume.sh
+++ train_script_resume.sh
@@ -1,5 +1,5 @@
-python ./training/pipelines/train_classifier.py --data-dir "D:/University And Papers/VESSL/dfdc_deepfake_challenge/data_root" \
- --config "D:/University And Papers/VESSL/dfdc_deepfake_challenge/configs/b7.json"\
- --output-dir "D:/University And Papers/VESSL/dfdc_deepfake_challenge/data_root/weights"\
- --resume "D:/University And Papers/VESSL/dfdc_deepfake_challenge/data_root/weights/classifier_DeepFakeClassifier_tf_efficientnet_b7_ns_1_last" \
- --logdir "D:/University And Papers/VESSL/dfdc_deepfake_challenge/logs"
+python ./training/pipelines/train_classifier.py --data-dir "./data_root" \
+ --config "./configs/b7.json"\
+ --output-dir "./data_root/weights"\
+ --resume "./data_root/weights/classifier_DeepFakeClassifier_tf_efficientnet_b7_ns_1_last" \
+ --logdir "./logs"
